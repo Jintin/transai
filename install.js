4895a788b409e98c1fd94f83cef0c64104112955
Jintin <jintin.lin1018@gmail.com>
2017-10-05 08:31:53 +0800

    lint warning update

--- install.js
+++ install.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-var execSync = require('child_process').execSync;
+var execSync = require("child_process").execSync;
 
 execSync("sudo npm uninstall -g");
 execSync("sudo npm install -g");
